654f8a2f3d5ae8af7145d00bee186b9b4866501e
ahnshy <ahnshy@naver.com>
2024-09-14 19:10:31 +0900

    <feat> : apply bootstrap v5.1.3 stable version <feat> : add static path asset/ <feat> : add main logo with relative path

--- InstagramClone/settings.py
+++ InstagramClone/settings.py
@@ -9,7 +9,7 @@ https://docs.djangoproject.com/en/4.2/topics/settings/
 For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.2/ref/settings/
 """
-
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -114,9 +114,13 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/4.2/howto/static-files/
+# https://docs.djangoproject.com/en/4.2/howto/assert-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = 'assert/'
+STATICFILES_DIRS = [
+    BASE_DIR / 'assert',
+    os.path.join(BASE_DIR, 'assert'),
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
