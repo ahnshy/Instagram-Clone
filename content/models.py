1b12509f8cf5c6e3018ef8ec16bdc94fbb2139ad
ahnshy <ahnshy@naver.com>
2024-09-16 14:51:11 +0900

    <feat> : insert sqllite feed table sampling tuple <fix> : add create contenct app <fix> : build warning error fixed

--- content/models.py
+++ content/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Feed(models.Model):
+    content = models.TextField()
+    image = models.TextField()
+    email = models.EmailField(default='')
+
+class Like(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    is_like = models.BooleanField(default=True)
+
+class Reply(models.Model):
+    feed_id = models.IntegerField(default=0)
+    email = models.EmailField(default='')
+    reply_content = models.TextField()
